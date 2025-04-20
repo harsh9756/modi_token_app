77fb8e3f0b3d89cde15580042f6a8a25c649e7ce
Harsh Upadhyay <harsh9756416665@gmail.com>
2025-04-20 10:38:03 +0530

    qr change

--- src/pages/PlayOnYourMobile.tsx
+++ src/pages/PlayOnYourMobile.tsx
@@ -1,14 +1,14 @@
 export default function PlayOnYourMobile() {
     return (
       <div className="flex flex-col items-center justify-center gap-6 h-svh">
-        <p className="text-2xl font-semibold uppercase">Play on your mobile</p>
+        <p className="text-2xl font-semibold uppercase">Play on Telegram!</p>
         <img
-          src="/images/qrcode.png"
+          src="/images/qrcode.jpg"
           alt="qrcode"
           className="object-contain max-w-xs rounded-xl"
         />
         <a href="#" className="text-2xl font-semibold">
-          @CoolFrogBot
+          @ModiCoinBot
         </a>
       </div>
     );
